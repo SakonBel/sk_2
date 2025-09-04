824a5a4c7f30d190c2c4a2b36f247c58db2a719f
Sakon Plangklang <iamsabell@gmail.com>
2025-09-05 01:25:03 +0700

    update inventory

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/skecherstmkorat/",
+  base: "/sk_2/",
   plugins: [
     vue(),
     VitePWA({
