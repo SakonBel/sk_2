c5b8a46b58667f18e1ca442ae4873008eb4594ce
Sakon Plangklang <iamsabell@gmail.com>
2025-09-10 20:11:04 +0700

    try PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/sk_2/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Skechers Korat",
+        short_name: "SKK",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "src/assets/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "src/assets/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "src/assets/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+    }),
+  ],
 });
